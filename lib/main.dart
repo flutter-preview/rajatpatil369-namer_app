0f9a1c98778e9ff29f794c10f8ba123e74c2493e
Rajat Patil <rajatpatil369@gmail.com>
2023-07-05 00:22:06 +0530

    Added bottom navigation.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:provider/provider.dart';
 
 void main() {
@@ -28,8 +27,13 @@ class MyApp extends StatelessWidget {
 
 class MyAppState extends ChangeNotifier {
   var current = WordPair.random();
+  var history = <WordPair>[];
+  GlobalKey? historyKeyList;
 
   void getNext() {
+    history.insert(0, current);
+    var animatedList = historyKeyList?.currentState as AnimatedListState?;
+    animatedList?.insertItem(0);
     current = WordPair.random();
     notifyListeners();
   }
@@ -44,6 +48,11 @@ class MyAppState extends ChangeNotifier {
     }
     notifyListeners();
   }
+
+  void removeFavorite(WordPair pair) {
+    favorites.remove(pair);
+    notifyListeners();
+  }
 }
 
 class MyHomePage extends StatefulWidget {
@@ -56,6 +65,8 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    var colorScheme = Theme.of(context).colorScheme;
+
     Widget page;
     switch (selectedIndex) {
       case 0:
@@ -68,44 +79,74 @@ class _MyHomePageState extends State<MyHomePage> {
         throw UnimplementedError('No widget for $selectedIndex.');
     }
 
-    return LayoutBuilder(builder: (context, constraints) {
-      return Scaffold(
-        floatingActionButton: null,
-        floatingActionButtonLocation: FloatingActionButtonLocation.startTop,
-        body: SafeArea(
-          child: Row(
-            children: [
-              NavigationRail(
-                extended: constraints.maxWidth >= 600,
-                destinations: [
-                  NavigationRailDestination(
-                    icon: Icon(Icons.home),
-                    label: Text('Home'),
-                  ),
-                  NavigationRailDestination(
-                    icon: Icon(Icons.favorite),
-                    label: Text('Favorites'),
-                  ),
-                ],
-                selectedIndex: selectedIndex,
-                onDestinationSelected: (value) {
-                  // print('selected: $value');
-                  setState(() {
-                    selectedIndex = value;
-                  });
-                },
-              ),
-              Expanded(
-                child: Container(
-                  color: Theme.of(context).colorScheme.primaryContainer,
-                  child: page,
+    var mainArea = ColoredBox(
+      color: colorScheme.surfaceVariant,
+      child: AnimatedSwitcher(
+        duration: Duration(milliseconds: 200),
+        child: page,
+      ),
+    );
+
+    return SafeArea(
+      child: LayoutBuilder(builder: (context, constraints) {
+        return Scaffold(
+          body: (constraints.maxWidth < 450)
+              ? Column(
+                  children: [
+                    Expanded(child: mainArea),
+                    BottomNavigationBar(
+                      items: [
+                        BottomNavigationBarItem(
+                          icon: Icon(Icons.home),
+                          label: 'Home',
+                        ),
+                        BottomNavigationBarItem(
+                          icon: Icon(Icons.favorite),
+                          label: 'Favorites',
+                        ),
+                      ],
+                      currentIndex: selectedIndex,
+                      onTap: (value) {
+                        setState(() {
+                          selectedIndex = value;
+                        });
+                      },
+                    )
+                  ],
+                )
+              : Row(
+                  children: [
+                    NavigationRail(
+                      extended: constraints.maxWidth >= 600,
+                      destinations: [
+                        NavigationRailDestination(
+                          icon: Icon(Icons.home),
+                          label: Text('Home'),
+                        ),
+                        NavigationRailDestination(
+                          icon: Icon(Icons.favorite),
+                          label: Text('Favorites'),
+                        ),
+                      ],
+                      selectedIndex: selectedIndex,
+                      onDestinationSelected: (value) {
+                        // print('selected: $value');
+                        setState(() {
+                          selectedIndex = value;
+                        });
+                      },
+                    ),
+                    Expanded(
+                      child: Container(
+                        color: Theme.of(context).colorScheme.primaryContainer,
+                        child: page,
+                      ),
+                    ),
+                  ],
                 ),
-              ),
-            ],
-          ),
-        ),
-      );
-    });
+        );
+      }),
+    );
   }
 }
 
@@ -168,32 +209,17 @@ class BigCard extends StatelessWidget {
     final theme = Theme.of(context);
     final style = theme.textTheme.displaySmall!
         .copyWith(color: theme.colorScheme.onPrimary);
-    final text = pair.asLowerCase;
-    // final textSpan = TextSpan(
-    //   text: text,
-    //   style: style,
-    // );
-    // final textPainter = TextPainter(
-    //   text: textSpan,
-    //   textDirection: TextDirection.ltr,
-    // );
 
     return Card(
       color: theme.colorScheme.primary,
       elevation: 8.0,
       child: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: LayoutBuilder(builder: (context, constraints) {
-          // textPainter.layout(maxWidth: constraints.maxWidth);
-          // if (textPainter.didExceedMaxLines) {
-          //   ???
-          // }
-          return Text(
-            text,
-            style: style,
-            semanticsLabel: '${pair.first} ${pair.second}',
-          );
-        }),
+        child: Text(
+          pair.asLowerCase,
+          style: style,
+          semanticsLabel: '${pair.first} ${pair.second}',
+        ),
       ),
     );
   }
@@ -217,9 +243,6 @@ class FavoritesPage extends StatelessWidget {
           child: Text('You have '
               '${appState.favorites.length} favorites:'),
         ),
-        // ...List<Widget>.from(
-        //     appState.favorites.map((e) => Text(e.asPascalCase))),
-        // for (var pair in appState.favorites) Text(pair.asPascalCase),
         for (var pair in appState.favorites)
           ListTile(
             leading: Icon(Icons.favorite),
